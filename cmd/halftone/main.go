70fd65dd13e1e8a8a57cfbef0c59c43de8ab85f6
thisisaaronland <devnull@localhost>
2020-02-15 18:48:28 -0800

    more better options handling

--- cmd/halftone/main.go
+++ cmd/halftone/main.go
@@ -14,11 +14,17 @@ import (
 
 func main() {
 
+	mode := flag.String("mode", "atkinson", "...")
+	scale := flag.Float64("scale-factor", 2.0, "...")
+
 	flag.Parse()
 
 	ctx := context.Background()
 
 	opts := halftone.NewDefaultHalftoneOptions()
+
+	opts.Mode = *mode
+	opts.ScaleFactor = *scale
 
 	cb := func(ctx context.Context, im image.Image, path string) (image.Image, string, error) {
 
@@ -33,13 +39,19 @@ func main() {
 		fname := filepath.Base(path)
 		ext := filepath.Ext(path)
 
-		label := "halftone"
-
 		short_name := strings.Replace(fname, ext, "", 1)
+
+		scale_label := fmt.Sprintf("%f", opts.ScaleFactor)
+		scale_label = strings.TrimRight(scale_label, "0")
+		scale_label = strings.TrimRight(scale_label, ".")
+
+		label := fmt.Sprintf("halftone-%s-%s", opts.Mode, scale_label)
+
 		new_name := fmt.Sprintf("%s-%s%s", short_name, label, ext)
 
 		new_path := filepath.Join(root, new_name)
 
+		log.Println(new_path)
 		return new_im, new_path, nil
 	}
 
--- transform.go
+++ transform.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"github.com/aaronland/go-image-transform"
 	"image"
-	_ "net/url"
+	"net/url"
+	"strconv"
 )
 
 type HalftoneTransformation struct {
@@ -24,7 +25,33 @@ func init() {
 
 func NewHalftoneTransformation(ctx context.Context, str_url string) (transform.Transformation, error) {
 
+	parsed, err := url.Parse(str_url)
+
+	if err != nil {
+		return nil, err
+	}
+
 	opts := NewDefaultHalftoneOptions()
+
+	query := parsed.Query()
+
+	mode := query.Get("mode")
+	str_scale := query.Get("scale-factor")
+
+	if mode != "" {
+		opts.Mode = mode
+	}
+
+	if str_scale != "" {
+
+		scale, err := strconv.ParseFloat(str_scale, 64)
+
+		if err != nil {
+			return nil, err
+		}
+
+		opts.ScaleFactor = scale
+	}
 
 	tr := &HalftoneTransformation{
 		options: opts,
